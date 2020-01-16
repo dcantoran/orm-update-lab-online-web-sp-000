dd6b5f182018cef933ec2711097b9cfab06bcf40
David Cantoran <davidcantoran36@gmail.com>
2020-01-16 20:29:30 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -27,8 +27,8 @@ class Student
   
   def save 
     sql = "INSERT INTO students (name, grade) VALUES (?, ?)"
-    DB[:conn].execute(sql)
-    @id = DB[:conn].execute(sql)
+    DB[:conn].execute(sql, self.name, self.grade)
+    # @id = DB[:conn].execute(sql)
   end 
 
 end
